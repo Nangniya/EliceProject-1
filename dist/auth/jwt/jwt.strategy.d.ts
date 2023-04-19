cecb8699f58035977a665cec615099b3e5833f79
bongsub kim <bong989971@gmail.com>
2023-04-19 20:09:51 +0900

    ignore

--- dist/auth/jwt/jwt.strategy.d.ts
+++
@@ -1,9 +0,0 @@
-import { UsersRepository } from 'src/users/users.repository';
-import { Payload } from './jwt.payload';
-declare const JwtStrategy_base: new (...args: any[]) => any;
-export declare class JwtStrategy extends JwtStrategy_base {
-    private readonly usersRepository;
-    constructor(usersRepository: UsersRepository);
-    validate(payload: Payload): Promise<import("../../users/users.schema").User>;
-}
-export {};
