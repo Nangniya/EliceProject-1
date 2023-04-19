a4a10d81ec296127c6f7452aa3eda75298c93a7e
[SW4/2반] 김나연 <apinksky00e@gmail.com>
2023-04-19 13:39:25 +0900

    Api요청 모듈화

--- views/register/register.js
+++ views/register/register.js
@@ -1,5 +1,8 @@
+import * as Api from "../api.js";
+
 submitButton.addEventListener("click", handleSubmit);
 
+// 이메일 형식 검사
 const validateEmail = (email) => {
   return String(email)
   .toLowerCase()
@@ -7,33 +10,6 @@ const validateEmail = (email) => {
     /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
   );
 };
-
-async function post(apiUrl, data) {
-// JSON.stringify 함수: Javascript 객체를 JSON 형태로 변환함.
-// 예시: {name: "Kim"} => {"name": "Kim"}
-const bodyData = JSON.stringify(data);
-
-const res = await fetch(apiUrl, {
-  method: "POST",
-  headers: {
-    "Content-Type": "application/json"
-  },
-  body: bodyData,
-});
-
-// 응답 코드가 4XX 계열일 때 (400, 403 등)
-if (!res.ok) {
-  const errorContent = await res.json();
-  const { reason } = errorContent;
-
-  throw new Error(reason);
-}
-
-const result = await res.json();
-
-return result;
-}
-
 
 
 // 요소(element), input 혹은 상수
@@ -76,12 +52,12 @@ async function handleSubmit(e) {
   try {
     const data = { email, name, password };
 
-    await post("http://localhost:8000/api/users/signup", data);
+    await Api.post("/api/users/signup", data);
 
     alert(`정상적으로 회원가입되었습니다.`);
 
     // 로그인 페이지 이동
-    window.location.href = "users/login";
+    window.location.href = "/api/users/login";
   } catch (err) {
     console.error(err.stack);
     alert(`문제가 발생하였습니다. 확인 후 다시 시도해 주세요: ${err.message}`);
