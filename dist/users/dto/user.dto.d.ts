2523d345934bd5ce642d043a383b36f3c60b3d3e
bongsub kim <bong989971@gmail.com>
2023-04-19 20:08:26 +0900

    ignore

--- dist/users/dto/user.dto.d.ts
+++
@@ -1,9 +0,0 @@
-import { User } from '../users.schema';
-declare const ReadOnlyUserDto_base: import("@nestjs/common").Type<Pick<User, "name" | "email">>;
-export declare class ReadOnlyUserDto extends ReadOnlyUserDto_base {
-    id: string;
-}
-declare const userIdDto_base: import("@nestjs/common").Type<Pick<ReadOnlyUserDto, "id">>;
-export declare class userIdDto extends userIdDto_base {
-}
-export {};
