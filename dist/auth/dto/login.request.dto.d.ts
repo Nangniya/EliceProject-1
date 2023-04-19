a5ca12a2e79b503eb97ed2d3c356b36972879a54
[SW4/2반] 김나연 <apinksky00e@gmail.com>
2023-04-19 21:28:32 +0900

    dist 지우기

--- dist/auth/dto/login.request.dto.d.ts
+++
@@ -1,5 +0,0 @@
-import { User } from 'src/users/users.schema';
-declare const LoginRequestDto_base: import("@nestjs/common").Type<Pick<User, "email" | "password">>;
-export declare class LoginRequestDto extends LoginRequestDto_base {
-}
-export {};
