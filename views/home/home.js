efdd3f25de7ac0b18181a3096e838bba2c2f38f7
Lee yong sub <dydtjq324@naver.com>
2023-04-21 14:52:26 +0900

    화면UI test

--- views/home/home.js
+++ views/home/home.js
@@ -1,24 +1,15 @@
 const serchbtn = document.querySelector('#search');
 const basketbtn = document.querySelector('#basket');
 const loginbtn = document.querySelector('#logout');
+const mypage = document.querySelector('#mypage');
 
 const btn1 = document.querySelector('.button1');
 const btn2 = document.querySelector('.button2');
 const btn3 = document.querySelector('.button3');
 const slide = document.querySelector('.slider-container');
 
-btn1.addEventListener('click', function () {
-  slide.style.transform = 'translate(0vw)';
-});
-btn2.addEventListener('click', function () {
-  slide.style.transform = 'translate(-100vw)';
-});
-btn3.addEventListener('click', function () {
-  slide.style.transform = 'translate(-200vw)';
-});
-
 async function fetchText() {
-  let response = await fetch('http://localhost:8000/api/products/recent');
+  let response = await fetch('/api/products/recent');
 
   console.log(response.status); // 200
   console.log(response); // 200
