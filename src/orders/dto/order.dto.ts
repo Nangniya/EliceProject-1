dbe2ba8bd2e450ac4a399204cf7591fead16c03e
bongsub kim <bong989971@gmail.com>
2023-04-21 13:24:51 +0900

    update delivery status, delete order

--- src/orders/dto/order.dto.ts
+++ src/orders/dto/order.dto.ts
@@ -1,6 +1,17 @@
-import { ApiProperty } from '@nestjs/swagger';
+import { ApiProperty, PickType } from '@nestjs/swagger';
+import { Order } from '../orders.schema';
 
 export class orderIdDto {
+  @ApiProperty({
+    example: '643f9dd9e8b6e97170c7529e',
+    description: 'order id',
+  })
+  id: string;
+}
+
+export class deliveryStatusDto extends PickType(Order, [
+  'deliveryStatus',
+] as const) {
   @ApiProperty({
     example: '643f9dd9e8b6e97170c7529e',
     description: 'order id',
