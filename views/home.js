d18209d8c2066d6b78550a7ff0d92abac1fe201b
Lee yong sub <dydtjq324@naver.com>
2023-04-24 21:16:49 +0900

    UI수정

--- views/home.js
+++ views/home.js
@@ -8,49 +8,75 @@ if (localStorage.getItem('token')) {
   console.log("있다");
 } else console.log('없다');
 
+// async function getCategories() {
+//   try {
+//     const response = await fetch('/api/categories');
+//     const data = await response.json();
 
-window.onload = function () {
-  category.classList.toggle('show');
-  fetch('/api/categories')
-    .then((response) => response.json())
-    .then((data) => {
-      var itemsHtml = '';
-      for (var i = 0; i < data.length; i++) {
-        itemsHtml += `
-        <button type="button" class="btn btn-warning category-title">${data[i].name}</button>
+//     var itemsHtml = '';
+//     for (var i = 0; i < data.length; i++) {
+//       itemsHtml += `
+//         <button type="button" class="btn btn-warning category_title">${data[i].name}</button>
+//       `;
+//     }
+
+//     document.querySelector('.items').innerHTML += itemsHtml;
+//   } catch (error) {
+//     console.error(error);
+//   }
+// }
+
+// getCategories();
+
+fetch('/api/categories')
+  .then((response) => response.json())
+  .then((data) => {
+    var itemsHtml = '';
+    for (var i = 0; i < data.length; i++) {
+      itemsHtml += `
+        <button type="button" class="btn btn-warning category_title">${data[i].name}</button>
           `;
-      }
+    }
 
-      document.querySelector('.items').innerHTML += itemsHtml;
-    })
-    .catch((error) => console.error(error));
+    document.querySelector('.items').innerHTML += itemsHtml;
+  })
+  .catch((error) => console.error(error));
 
-
-  fetch('/api/products/recent')
-    .then((response) => response.json())
-    .then((data) => {
-      var itemsHtml = '';
-      for (var i = 0; i < data.length; i++) {
-        itemsHtml += `<button class="category-item">${data[i].name}</button>
-          `;
-      }
-
-      document.querySelector('.new-item-conatiner').innerHTML = itemsHtml;
-    });
-
-  fetch('/api/products')
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(data);
-      var itemsHtml = '';
-      for (var i = 0; i < data.length; i++) {
-        itemsHtml += `<div>
-            <p>상품명 : ${data[i].name}<p>
-            <p>가격 : ${data[i].price}<p>
+fetch('/api/products/recent')
+  .then((response) => response.json())
+  .then((data) => {
+    var itemsHtml = '';
+    for (var i = 0; i < data.length; i++) {
+      itemsHtml += `<div>
+            <p class="item-title">상품명 : ${data[i].name}<p>
+            <p class="item-price">가격 : ${data[i].price}<p>
             </div>
           `;
-      }
+    }
 
-      document.querySelector('.best-item-conatiner').innerHTML = itemsHtml;
-    });
-};
+    document.querySelector('.new-item-conatiner').innerHTML = itemsHtml;
+  });
+
+fetch('/api/products')
+  .then((response) => response.json())
+  .then((data) => {
+    // console.log(data);
+    var itemsHtml = '';
+    for (var i = 0; i < data.length; i++) {
+      itemsHtml += `<div>
+            <p class="item-title">상품명 : ${data[i].name}<p>
+            <p class="item-price">가격 : ${data[i].price}<p>
+            </div>
+          `;
+    }
+
+    document.querySelector('.best-item-conatiner').innerHTML = itemsHtml;
+  });
+
+function handleButtonClick(event) {
+  console.log('asdf');
+}
+
+document.querySelectorAll('.category_title').forEach((button) => {
+  button.addEventListener('click', handleButtonClick);
+});
