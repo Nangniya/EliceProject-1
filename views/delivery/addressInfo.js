8ea30a5f310f49e84c85770f6998970345ee3879
SAEJIN PARK <cobaltcyan.park@gmail.com>
2023-04-25 19:58:53 +0900

    orderDefault 수정

--- views/delivery/addressInfo.js
+++ views/delivery/addressInfo.js
@@ -16,6 +16,37 @@ const btnCancel = document.querySelector('#btnCancel');
 const btnSave = document.querySelector('#btnSave');
 const btnAddressInfoSet = document.querySelector('#addressInfoSetting');
 
+
+async function getUser(){
+    const res = await fetch('http://localhost:8000/api/users',{
+        headers: {
+        Authorization: `Bearer ${localStorage.getItem('token')}`
+        }
+    });
+    if(!res.ok) {
+        const errorContent = await res.json();
+        const { reason } = errorContent;
+
+        throw new Error(reason);
+
+    }
+    const data = await res.json();
+    console.log(data);  
+    // console.log(data.data.name); 
+    // console.log(data.data.email); 
+
+    let name = data.data.name;
+    let email = data.data.email;
+
+    document.getElementById('name').innerHTML = name;
+    // document.getElementById('user-info-content-email').innerHTML = email;
+
+}
+
+
+getUser();
+
+
 btnCancel.addEventListener('click', function() {
     window.location = document.referrer;
 });
@@ -26,6 +57,5 @@ btnSave.addEventListener('click', function() {
 
 btnAddressInfoSet.addEventListener('click', function() {
     // 배송지 정보 설정하는 새 창 띄우기
-    alert('새창 띄우기');
+    alert('주소조회');
 });
-
--- views/order/orderDefault.js
+++ views/order/orderDefault.js
@@ -1,8 +1,8 @@
-
-
-const btnOrderCancel = document.querySelector('#btnOrderCancel');
+const btnOrderCreateCancel = document.querySelector('#btnOrderCreateCancel');
+const btnOrderDetailCancel = document.querySelector('#btnOrderDetailCancel');
 const btnOrderConfirm = document.querySelector('#btnOrderConfirm');
 const btnMoveCart = document.querySelector('#btnMoveCart');
+const btnMoveOrderList = document.querySelector('#btnMoveOrderList');
 const btnAddressInfo = document.querySelector('#btnAddressInfo');
 
 const btnGroupOrderCreate = document.querySelector('.order-create');
@@ -24,9 +24,20 @@ if(urlOrderId !== null && urlOrderId !== undefined) {
     btnGroupOrderDetail.style.display = "none";
 }
 
-btnOrderCancel.addEventListener('click', function() {
-    //홈 화면으로 돌아가기
+btnOrderCreateCancel.addEventListener('click', function() {
     window.location.href = "../index.html";
+});
+
+btnOrderDetailCancel.addEventListener('click', function() {
+    window.location.href = "../index.html";
+});
+
+btnMoveCart.addEventListener('click', function() {
+    window.location.href = "../cart";
+});
+
+btnMoveOrderList.addEventListener('click', function() {
+    window.location.href = "../order/orderList.html";
 });
 
 btnOrderConfirm.addEventListener('click', function() {
@@ -47,51 +58,96 @@ btnMoveCart.addEventListener('click', function() {
 btnAddressInfo.addEventListener('click', function() {
     //주소록 목록이 띄워지면 좋겠는데 우선은 이걸로 한다
     window.open("/delivery/addressInfo.html"
-    , "addressInfo", "top=250, left=400, width=750, height=500, toolbar=no, menubar=no, scrollbars=yes, resizeable=no");
+    , "addressInfo", "top=250, left=400, width=650, height=350, toolbar=no, menubar=no, scrollbars=yes, resizeable=no");
 });
 
 
-//주문자 정보 가져오기
-function getCurrentUser () {
-    fetch('http://localhost:8000/api/users')
-    .then((response) => response.json())
-    .then((data) => {
-        
-        // console.log(data);
-        let name = data.name;
-        let email = data.email;
-        let address = data.address;
-        let phoneNum = data.phoneNum;
-
-        document.getElementById('user-info-content-name') = name;
-        document.getElementById('user-info-content-email') = email;
-        document.getElementById('user-info-content-address') = address;
-        document.getElementById('user-info-content-phoneNum') = phoneNum;
-
+async function getUser(){
+    const res = await fetch('http://localhost:8000/api/users',{
+        headers: {
+        Authorization: `Bearer ${localStorage.getItem('token')}`
+        }
     });
-    
+    if(!res.ok) {
+        const errorContent = await res.json();
+        const { reason } = errorContent;
+
+        throw new Error(reason);
+
+    }
+    const data = await res.json();
+    console.log(data);  
+    // console.log(data.data.name); 
+    // console.log(data.data.email); 
+
+    let name = data.data.name;
+    let email = data.data.email;
+
+    document.getElementById('user-info-content-name').innerHTML = name;
+    document.getElementById('user-info-content-email').innerHTML = email;
+
 }
+
+
+// 주문자 정보 가져오기
+// function getCurrentUser () {
+//     fetch('http://localhost:8000/api/users')
+//     .then((response) => response.json())
+//     .then((data) => {
+        
+//         // console.log(data);
+//         let name = data.name;
+//         let email = data.email;
+//         let address = data.address;
+//         let phoneNum = data.phoneNum;
+
+//         document.getElementById('user-info-content-name') = name;
+//         document.getElementById('user-info-content-email') = email;
+//         document.getElementById('user-info-content-address') = address;
+//         document.getElementById('user-info-content-phoneNum') = phoneNum;
+
+//     });
+    
+// }
 
 //단가 콤마 반영
 function priceToString(price) {
     return price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
 }
 
-//상품 정보 가져오기
-function getProductInfo () {
-    fetch('http://localhost:8000/api/products')
-    .then((response) => response.json())
-    .then((data) => {
-        console.log(data);   
-    }
-}
+// 상품 정보 가져오기
+// function getProductInfo(productId) {
+
+//     fetch('http://localhost:8000/api/products')
+//     .then((response) => response.json())
+//     .then((product) => {
+//         // console.log(product);
+//         // console.log(product[0]);
+//         // console.log(product[1]);
+    
+//         console.log(product[0].productId);
+//         console.log(product[0].name);
+//         console.log(product[0].price);
+//         console.log(product[0].imgUrl);
+//         console.log(product[0].content);
+//     });
+
+// // }
+
 
 function getUserOrderList () {
 
     fetch('http://localhost:8000/api/orders')
+    //fetch('http://localhost:8000/api/orders/getByOrderId')
     .then((response) => response.json())
     .then((data) => {
+        console.log(data);
     
+        // console.log(data[0]);
+        // console.log(data[0].orderedProducts);
+        // console.log(data[0].orderedProducts[0].productId);
+        // console.log(data[0].orderedProducts[0].quantity);
+        
         for (let i = 0; i < data.length; i++) {
 
             console.log(data[i]);
@@ -101,32 +157,40 @@ function getUserOrderList () {
             let deliveryStatus = data[i].deliveryStatus;
             let createAt = data[i].createdAt;
             let price = data[i].price;
-            let quantity = data[i].quantity;
-            let supplyPrice = data[i].price * quantity;     //공급가
-            let orderDate = createAt.substr(0, 10);
+            // let quantity = data[i].quantity;
+            let orderedProducts = data[i];                  //주문한 품목들
 
-            let temp_html = '';
-        
-            temp_html += `<div class="order-detail-content">
-                            <ul>
-                                <li id="productImage"><img src="${productImage}" /></li>
-                                <li id="productName">케로케로케로피 페이스 쿠션</li>
-                                |
-                                <li id="quantity">${quantity}개</li>
-                                |
-                                <li id="price">${price}원</li>
-                                |
-                                <li id="supplyPrice">${supplyPrice}원</li>
-                            </ul>
-                            </div>`;
-    
+            // //주문번호에 따른 상품목록
+            // for (let j = 0; j < data.length; j++) {
 
-        }
+            //     console.log(data[i].orderedProducts[j]);
+            //     console.log(data[i].orderedProducts[j].productId);      //왜 안되지?
+            //     console.log(data[i].orderedProducts[j].quantity);
+            // }
+            
+            // let supplyPrice = data[i].price * quantity;     //공급가
+            // let orderDate = createAt.substr(0, 10);
+
+            // let temp_html = '';
         
+            // temp_html += `<div class="order-detail-content">
+            //                 <ul>
+            //                     <li id="productImage"><img src="${productImage}" /></li>
+            //                     <li id="productName">${productName}</li>
+            //                     |
+            //                     <li id="quantity">${quantity}개</li>
+            //                     |
+            //                     <li id="price">${price}원</li>
+            //                     |
+            //                     <li id="supplyPrice">${supplyPrice}원</li>
+            //                 </ul>
+            //             </div>`;   
+
+        }        
     });
 }
 
 
-getCurrentUser();
+getUser();
 getUserOrderList();
 
