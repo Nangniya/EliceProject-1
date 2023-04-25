4b03aeab0f823c4f6a8afcbb0b4c4fb7ea394b08
bongsub kim <bong989971@gmail.com>
2023-04-25 17:44:14 +0900

    detail go

--- views/home.js
+++ views/home.js
@@ -1,20 +1,8 @@
-// const searchbtn = document.querySelector('.searchbtn');
-// const searchinput = document.querySelector('.searchinput');
-
-const basketbtn = document.querySelector('#basket');
-const loginbtn = document.querySelector('#login');
-const mypage = document.querySelector('#mypage');
-const category = document.querySelector('#myCategory');
-
-if (localStorage.getItem('token')) {
-  console.log('있다');
-} else console.log('없다');
-
 fetch('/api/categories')
   .then((response) => response.json())
   .then((data) => {
-    var itemsHtml = '';
-    for (var i = 0; i < data.length; i++) {
+    let itemsHtml = '';
+    for (let i = 0; i < data.length; i++) {
       itemsHtml += `
         <button type="button" class="btn btn-warning category_title">${data[i].name}</button>
           `;
@@ -23,13 +11,11 @@ fetch('/api/categories')
     document.querySelector('.items').innerHTML += itemsHtml;
 
     function handleButtonClick(event) {
-      // console.log(event.target.innerText);
       let testhtml = '';
       fetch('/api/products')
         .then((response) => response.json())
         .then((data) => {
           for (let i = 0; i < data.length; i++) {
-            // console.log(data[i].category);
             if (event.target.innerText == data[i].category) {
               testhtml += `<div>
               <p class="item-title">상품명 : ${data[i].name}<p>
@@ -56,9 +42,9 @@ fetch('/api/categories')
 fetch('/api/products/recent')
   .then((response) => response.json())
   .then((data) => {
-    var itemsHtml = '';
-    for (var i = 0; i < data.length; i++) {
-      itemsHtml += `<div>
+    let itemsHtml = '';
+    for (let i = 0; i < data.length; i++) {
+      itemsHtml += `<div class=${data[i].name}>
             <p class="item-title">상품명 : ${data[i].name}<p>
             <p class="item-price">가격 : ${data[i].price}<p>
             </div>
@@ -70,31 +56,20 @@ fetch('/api/products/recent')
 fetch('/api/products')
   .then((response) => response.json())
   .then((data) => {
-    // console.log(data);
-    var itemsHtml = '';
-    for (var i = 0; i < data.length; i++) {
-      itemsHtml += `<div>
+    let itemsHtml = '';
+    for (let i = 0; i < data.length; i++) {
+      itemsHtml += `<div id=${data[i]._id}>
             <p class="item-title">상품명 : ${data[i].name}<p>
             <p class="item-price">가격 : ${data[i].price}<p>
             </div>
           `;
     }
     document.querySelector('.best-item-conatiner').innerHTML = itemsHtml;
+    for (let i = 0; i < data.length; i++) {
+      document
+        .getElementById(`${data[i]._id}`)
+        .addEventListener('click', () => {
+          window.location.href = `/detail/?id=${data[i]._id}`;
+        });
+    }
   });
-
-// 아이템 검색
-
-// searchbtn.addEventListener('click', function (e) {
-//   e.defaultPrevented;
-//   fetch('/api/products')
-//     .then((response) => response.json())
-//     .then((data) => {
-//       for (let i = 0; i < data.length; i++) {
-//         if (searchinput.value == data[i].name) {
-//           alert('상품 있음 해당상품페이지로 이동');
-//           return;
-//         }
-//       }
-//       alert('상품이 없슴!');
-//     });
-// });
