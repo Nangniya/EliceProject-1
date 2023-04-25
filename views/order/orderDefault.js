d57e7837b753a549a32609a27b5bb16bb088ed80
SAEJIN PARK <cobaltcyan.park@gmail.com>
2023-04-25 17:16:28 +0900

    orderDefault orderConfirm orderList 파일 수정

--- views/order/orderDefault.js
+++ views/order/orderDefault.js
@@ -1,8 +1,28 @@
 
+
 const btnOrderCancel = document.querySelector('#btnOrderCancel');
 const btnOrderConfirm = document.querySelector('#btnOrderConfirm');
 const btnMoveCart = document.querySelector('#btnMoveCart');
 const btnAddressInfo = document.querySelector('#btnAddressInfo');
+
+const btnGroupOrderCreate = document.querySelector('.order-create');
+const btnGroupOrderDetail = document.querySelector('.order-detail');
+
+let urlSearch = "";
+let urlOrderId = "";
+
+urlSearch = new URLSearchParams(location.search);
+urlOrderId = urlSearch.get('orderId');
+
+if(urlOrderId !== null && urlOrderId !== undefined) {
+    // null 및 undefined가 아닌 경우 실행되는 로직 - 주문상세페이지
+    btnGroupOrderCreate.style.display = "none";
+    btnGroupOrderDetail.style.display = "block";
+} else {
+    // null 및 undefined인 경우 실행되는 로직 - 주문/결제페이지
+    btnGroupOrderCreate.style.display = "block";
+    btnGroupOrderDetail.style.display = "none";
+}
 
 btnOrderCancel.addEventListener('click', function() {
     //홈 화면으로 돌아가기
@@ -16,7 +36,8 @@ btnOrderConfirm.addEventListener('click', function() {
     if (confirm (confirmMsg)) {
         alert('주문이 완료되었습니다.');
         window.location.href = "./orderConfirm.html";
-    }});
+    }
+});
 
 btnMoveCart.addEventListener('click', function() {
     //장바구니로 돌아가기
@@ -30,7 +51,82 @@ btnAddressInfo.addEventListener('click', function() {
 });
 
 
+//주문자 정보 가져오기
+function getCurrentUser () {
+    fetch('http://localhost:8000/api/users')
+    .then((response) => response.json())
+    .then((data) => {
+        
+        // console.log(data);
+        let name = data.name;
+        let email = data.email;
+        let address = data.address;
+        let phoneNum = data.phoneNum;
+
+        document.getElementById('user-info-content-name') = name;
+        document.getElementById('user-info-content-email') = email;
+        document.getElementById('user-info-content-address') = address;
+        document.getElementById('user-info-content-phoneNum') = phoneNum;
+
+    });
+    
+}
+
+//단가 콤마 반영
+function priceToString(price) {
+    return price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+}
+
+//상품 정보 가져오기
+function getProductInfo () {
+    fetch('http://localhost:8000/api/products')
+    .then((response) => response.json())
+    .then((data) => {
+        console.log(data);   
+    }
+}
+
+function getUserOrderList () {
+
+    fetch('http://localhost:8000/api/orders')
+    .then((response) => response.json())
+    .then((data) => {
+    
+        for (let i = 0; i < data.length; i++) {
+
+            console.log(data[i]);
+            
+            let orderId = data[i]._id;
+            let userId = data[i].userId;
+            let deliveryStatus = data[i].deliveryStatus;
+            let createAt = data[i].createdAt;
+            let price = data[i].price;
+            let quantity = data[i].quantity;
+            let supplyPrice = data[i].price * quantity;     //공급가
+            let orderDate = createAt.substr(0, 10);
+
+            let temp_html = '';
+        
+            temp_html += `<div class="order-detail-content">
+                            <ul>
+                                <li id="productImage"><img src="${productImage}" /></li>
+                                <li id="productName">케로케로케로피 페이스 쿠션</li>
+                                |
+                                <li id="quantity">${quantity}개</li>
+                                |
+                                <li id="price">${price}원</li>
+                                |
+                                <li id="supplyPrice">${supplyPrice}원</li>
+                            </ul>
+                            </div>`;
+    
+
+        }
+        
+    });
+}
 
 
-
+getCurrentUser();
+getUserOrderList();
 
