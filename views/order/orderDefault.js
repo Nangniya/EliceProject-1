b33b0fab9fc097397860556afc8f46ff67abf36f
SAEJIN PARK <cobaltcyan.park@gmail.com>
2023-04-27 00:06:27 +0900

    orderDefault 파일

--- views/order/orderDefault.js
+++ views/order/orderDefault.js
@@ -37,13 +37,55 @@ btnMoveOrderList.addEventListener('click', function () {
   window.location.href = '../order/index.html';
 });
 
-btnOrderConfirm.addEventListener('click', function () {
-  const confirmMsg = '결제하시겠습니까?';
 
-  if (confirm(confirmMsg)) {
-    alert('주문이 완료되었습니다.');
-    window.location.href = './orderConfirm.html';
-  }
+
+btnOrderConfirm.addEventListener('click', function () {
+
+    const confirmMsg = '결제하시겠습니까?';
+
+    let input_userName = document.getElementById('user-info-content-name').innerHTML;
+    let input_address = document.getElementById('user-info-content-address').innerHTML;
+    let input_phoneNum = document.getElementById('user-info-content-phoneNumber').innerHTML;
+    let input_receiver = document.getElementById('user-info-content-name').innerHTML;
+    let input_deliveryMessage = document.getElementById('address-content-select-wrapper-message').value;
+
+    // let input_orderedProducts = {};
+    // let input_price = {};
+
+    if (confirm(confirmMsg)) {
+
+        // alert('주문이 완료되었습니다.');
+        // window.location.href = './orderConfirm.html';
+
+            fetch("http://localhost:8000/api/orders", {
+            method: "POST", // POST
+            headers: { // 헤더 조작
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+    
+                userId: "643e1ada43da3cb65097f989",
+                address: "대전 가양동",
+                phoneNum: "010-0000-0000",
+                receiver: "kim",
+                deliveryMessage: "safe please",
+                orderedProducts: [
+                    {
+                        "productId": "643e4d7dcd5d39e480d32032",
+                        "quantity": 10
+                    }
+                ],
+                price: 10000
+                
+            }),
+        })
+        .then((response) => response.json())
+        .then((data) => console.log(data))
+        
+    } else {
+
+        return 0;
+    }
 });
 
 btnMoveCart.addEventListener('click', function () {
@@ -64,7 +106,9 @@ async function getUser() {
 
     throw new Error(reason);
   }
+
   const data = await res.json();
+
   let name = data.data.name;
   let email = data.data.email;
   let address = data.data.address;
@@ -123,6 +167,8 @@ function getUserOrderList(urlOrderId) {
     fetch('http://localhost:8000/api/orders/getByOrderId/' + urlOrderId)
       .then((response) => response.json())
       .then((data) => {
+        console.log(data);
+
         // let orderId = urlOrderId;
         let userId = '';
         let address = '';
@@ -191,50 +237,50 @@ function sample6_execDaumPostcode() {
     oncomplete: function (data) {
       // 팝업에서 검색결과 항목을 클릭했을때 실행할 코드를 작성하는 부분.
 
-      // 각 주소의 노출 규칙에 따라 주소를 조합한다.
-      // 내려오는 변수가 값이 없는 경우엔 공백('')값을 가지므로, 이를 참고하여 분기 한다.
-      var addr = ''; // 주소 변수
-      var extraAddr = ''; // 참고항목 변수
+    // 각 주소의 노출 규칙에 따라 주소를 조합한다.
+    // 내려오는 변수가 값이 없는 경우엔 공백('')값을 가지므로, 이를 참고하여 분기 한다.
+    var addr = ''; // 주소 변수
+    var extraAddr = ''; // 참고항목 변수
 
-      //사용자가 선택한 주소 타입에 따라 해당 주소 값을 가져온다.
-      if (data.userSelectedType === 'R') {
+    //사용자가 선택한 주소 타입에 따라 해당 주소 값을 가져온다.
+    if (data.userSelectedType === 'R') {
         // 사용자가 도로명 주소를 선택했을 경우
         addr = data.roadAddress;
-      } else {
+    } else {
         // 사용자가 지번 주소를 선택했을 경우(J)
         addr = data.jibunAddress;
-      }
+    }
 
-      // 사용자가 선택한 주소가 도로명 타입일때 참고항목을 조합한다.
-      if (data.userSelectedType === 'R') {
+    // 사용자가 선택한 주소가 도로명 타입일때 참고항목을 조합한다.
+    if (data.userSelectedType === 'R') {
         // 법정동명이 있을 경우 추가한다. (법정리는 제외)
         // 법정동의 경우 마지막 문자가 "동/로/가"로 끝난다.
         if (data.bname !== '' && /[동|로|가]$/g.test(data.bname)) {
-          extraAddr += data.bname;
+        extraAddr += data.bname;
         }
         // 건물명이 있고, 공동주택일 경우 추가한다.
         if (data.buildingName !== '' && data.apartment === 'Y') {
-          extraAddr +=
+        extraAddr +=
             extraAddr !== '' ? ', ' + data.buildingName : data.buildingName;
         }
         // 표시할 참고항목이 있을 경우, 괄호까지 추가한 최종 문자열을 만든다.
         if (extraAddr !== '') {
-          extraAddr = ' (' + extraAddr + ')';
+        extraAddr = ' (' + extraAddr + ')';
         }
         // 조합된 참고항목을 해당 필드에 넣는다. --> 없어서 주석처리
         // document.getElementById("sample6_extraAddress").value = extraAddr;
-      } else {
+    } else {
         //--> 없어서 주석처리
         // document.getElementById("sample6_extraAddress").value = '';
-      }
+    }
 
-      // 우편번호와 주소 정보를 해당 필드에 넣는다.
-      document.getElementById('sample6_postcode').value = data.zonecode;
-      document.getElementById('sample6_address').value = addr;
-      // 커서를 상세주소 필드로 이동한다.
-      document.getElementById('sample6_detailAddress').focus();
+    // 우편번호와 주소 정보를 해당 필드에 넣는다.
+    document.getElementById('sample6_postcode').value = data.zonecode;
+    document.getElementById('sample6_address').value = addr;
+    // 커서를 상세주소 필드로 이동한다.
+    document.getElementById('sample6_detailAddress').focus();
     },
-  }).open();
+}).open();
 }
 
 getUser();
@@ -243,3 +289,4 @@ getUser();
 
 // getProductInfo('644627867145207d8a0ff1b7');
 getUserOrderList(urlOrderId);
+
